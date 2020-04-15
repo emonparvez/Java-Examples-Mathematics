6456ae9bcbc170fd3fe2fb81c9fa563bc7a692b1
Emon Parvez <32875754+emonparvez@users.noreply.github.com>
2020-04-16 04:23:52 +0600

    Java Program to print Fibonacci Series

---
+++ src/java_example_mathematics/Fibonaicci_Number.java
@@ -0,0 +1,24 @@
+
+package java_example_mathematics;
+
+
+public class Fibonaicci_Number {
+    
+    
+    public static void main(String[] args) {
+        int first=0;
+        int second=1;
+        int i = 0;
+        System.out.print(first + " " + second+ " ");
+        
+        while(i <100){
+            
+            i=first+second;
+            System.out.print(i +" ");
+            first=second;
+            second=i;
+            
+            
+        }
+    }
+}
