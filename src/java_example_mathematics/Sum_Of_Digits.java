20b667c1cf1719bc321c9d63b300bb0b94b41b47
Emon Parvez <32875754+emonparvez@users.noreply.github.com>
2020-04-16 04:32:05 +0600

    Java Program to print Sum of Digits

---
+++ src/java_example_mathematics/Sum_Of_Digits.java
@@ -0,0 +1,28 @@
+
+package java_example_mathematics;
+
+import java.util.Scanner;
+
+public class Sum_Of_Digits {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        int n= sc.nextInt();
+        
+        int sum=0,r;
+       
+       
+       
+       
+        while(n!=0){
+            r=n%10;
+            sum=sum+r;
+            n=n/10;
+        }
+        System.out.println("Sum of digits is : " + sum);
+    }
+    
+    
+    
+}
